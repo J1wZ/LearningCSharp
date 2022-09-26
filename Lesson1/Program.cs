1b88f4f03e46a1536c027fbc88e7bb828c556be3
stud <stud@stud.sud>
2022-09-26 11:46:46 +0300

    Add source files

---
+++ Lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static int Age;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
